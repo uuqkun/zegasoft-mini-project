9ed8e53ad9e37bfaa53c06c758d09e4cb4913a06
Achmad Furqon Rachmadie <uuqkun@gmail.com>
2024-03-22 17:44:59 +0700

    finished Excercise-2 - Added 3rd fn to formatNumber()

--- src/excercise-2/greet.ts
+++ src/excercise-2/greet.ts
@@ -1,5 +1,7 @@
 function greet(time: string): string {
     let message: string = "";
+
+    // Get the hour from the time
     const hour = parseFloat(time.split('.')[0]);
 
     if (hour >= 0 && hour < 23) {
@@ -18,12 +20,43 @@ function greet(time: string): string {
 }
 
 function countCharacters(sentence: string): string {
+    // Remove whitespaces and count the characters
     const characters = sentence.replace(/\s/g, '').length;
 
     return `Jumlah huruf adalah ${characters} karakter.`;
 }
 
+function formatNumbers(input: number): number {
+    // Convert input to string
+    const rounded = input.toFixed(2);
+
+    // Split the number into base and decimal
+    const [base, decimal] = rounded.split('.');
+
+    // Get the first number of the decimal
+    const decimalNumber = Math.round(parseFloat(`${decimal[0]}.${decimal[1]}`));
+
+    // Convert decimal to float if needed
+    // const formattedDecimal = parseFloat(`0.${decimalNumber}`);
+
+    // Convert base to float
+    const formattedBase = parseFloat(base);
+
+    let output: number;
+
+    if (decimalNumber >= 0 && decimalNumber <= 3) {
+        output = formattedBase;
+    } else if (decimalNumber <= 7) {
+        output = formattedBase + 0.5;
+    } else {
+        output = formattedBase + 1;
+    }
+
+    return output;
+}
+
 export {
+    formatNumbers,
     countCharacters,
     greet
 }
--- tests/excercise-2.test.ts
+++ tests/excercise-2.test.ts
@@ -1,4 +1,4 @@
-import { countCharacters, greet } from '../src/excercise-2/greet';
+import { countCharacters, formatNumbers, greet } from '../src/excercise-2/greet';
 
 describe('greet.ts functions', () => {
     it('should return a proper value', () => {
@@ -14,5 +14,43 @@ describe('greet.ts second function', () => {
         const test = countCharacters('of character within');
 
         expect(test).toBe('Jumlah huruf adalah 17 karakter.')
-    })
+    });
+});
+
+describe('greet.ts third function', () => {
+    it('Should return base number only', () => {
+        const numbers: number[] = [3.23, 3.14, 3.22, 3.32];
+        const test: number[] = [];
+
+        numbers.map(num => {
+            test.push(formatNumbers(num));
+        });
+
+        expect(test).toBeInstanceOf(Object);
+        expect(test).toStrictEqual([3, 3, 3, 3]);
+    });
+
+    it('Should return base number + 0.5', () => {
+        const numbers: number[] = [5.41, 5.37, 5.67, 5.72];
+        const test: number[] = [];
+
+        numbers.map(num => {
+            test.push(formatNumbers(num));
+        });
+
+        expect(test).toBeInstanceOf(Object);
+        expect(test).toStrictEqual([5.5, 5.5, 5.5, 5.5]);
+    });
+
+    it('Should return base number + 1', () => {
+        const numbers: number[] = [10.76, 10.82, 10.97, 10.99];
+        const test: number[] = [];
+
+        numbers.map(num => {
+            test.push(formatNumbers(num));
+        });
+
+        expect(test).toBeInstanceOf(Object);
+        expect(test).toStrictEqual([11, 11, 11, 11]);
+    });
 });
